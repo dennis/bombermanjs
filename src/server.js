a370d68c0e92548af5575949b5982283dcce2696
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2013-12-28 21:54:21 +0100

    Loads and shows a dummy background
    - Getting the hang of javascript and nodejs.
    - Added a level
    - Minimal server.js

---
+++ src/server.js
@@ -0,0 +1,39 @@
+var server_port = 8888;
+var app = require('http').createServer(handler)
+	, io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(server_port);
+
+console.log("Server running on port " + server_port);
+
+var level = require(__dirname + '/../assets/levels/level.json');
+
+function handler(request, response) {
+	console.log(request.method, request.url);
+	switch(request.url) {
+	case '/':
+		serveFile(response, __dirname + '/index.html');
+		break;
+	case '/favicon.ico':
+		response.writeHead(404);
+		break;
+	}
+}
+
+function serveFile(response, filename) {
+	fs.readFile(filename, function (err, data) {
+			if (err) {
+					response.writeHead(500);
+					return response.end('Error loading index.html');
+			}
+
+			response.writeHead(200);
+			response.end(data);
+	});
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('new-level', level );
+});
+
