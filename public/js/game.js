098dc3b64b27b942c81527ac1f6f28ac6ee4c23b
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2013-12-30 21:07:13 +0100

    Move game initialization into game.js

--- public/js/game.js
+++ public/js/game.js
@@ -50,3 +50,19 @@ Game.prototype.run = function() {
 		self.render();
 	};
 }
+
+function renderer() {
+	requestAnimFrame(renderer);
+	game.run()();
+}
+
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	function(/* function */ callback, /* DOMElement */ element){
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
--- public/js/main.js
+++ public/js/main.js
@@ -37,19 +37,3 @@ function sendActions() {
 var game = new Game(input);
 game.addRender(function() { level.render(); });
 game.addLogic(function() { sendActions(); });
-
-function renderer() {
-	requestAnimFrame(renderer);
-	game.run()();
-}
-
-window.requestAnimFrame = (function(){
-	return window.requestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	window.oRequestAnimationFrame ||
-	window.msRequestAnimationFrame ||
-	function(/* function */ callback, /* DOMElement */ element){
-		window.setTimeout(callback, 1000 / 60);
-	};
-})();
