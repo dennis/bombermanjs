b4f4cea090a00a0dd43732bcc6260c85f60a71c3
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2013-12-30 02:07:58 +0100

    Use expressjs for routing. Had to duplicate tileset for it to work
    expressjs refused to serve anything outside its root directory

--- src/server.js
+++ src/server.js
@@ -1,40 +1,21 @@
 var server_port = 8888;
-var app = require('http').createServer(handler)
-	, io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('express')()
+	, server = require('http').createServer(app)
+	, io = require('socket.io').listen(server)
+	, fs = require('fs')
 
-app.listen(server_port);
+server.listen(server_port);
 
 console.log("Server running on port " + server_port);
 
 var level = require(__dirname + '/../assets/levels/level.json');
 
-function handler(request, response) {
-	console.log(request.method, request.url);
-	switch(request.url) {
-	case '/':
-		serveFile(response, __dirname + '/index.html');
-		break;
-	case '/favicon.ico':
-		response.writeHead(404);
-		break;
-	case '/img/bomb_party_v4.png':
-		serveFile(response, __dirname + '/../assets/img/bomb_party_v4.png');
-		break;
-	}
-}
-
-function serveFile(response, filename) {
-	fs.readFile(filename, function (err, data) {
-			if (err) {
-					response.writeHead(500);
-					return response.end('Error loading index.html');
-			}
-
-			response.writeHead(200);
-			response.end(data);
-	});
-}
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+app.get('/img/bomb_party_v4.png', function(req, res) {
+	res.sendfile(__dirname + '/img/bomb_party_v4.png');
+});
 
 var players = {};
 var playerKeys = [];
