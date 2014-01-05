0ee00d0184f451bc33feeeb7c1e57e67b2f4db11
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2014-01-06 00:16:52 +0100

    Ignore key-repeats

--- public/js/input_manager.js
+++ public/js/input_manager.js
@@ -12,6 +12,7 @@ function InputManager() {
 
 	this.oldonkeydown = null;
 	this.oldonkeyup = null;
+	this.ignoreRepeats = false;
 }
 InputManager.prototype.resetKeys = function() {
 	for(code in this.KEY_CODES) {
@@ -21,6 +22,9 @@ InputManager.prototype.resetKeys = function() {
 InputManager.prototype.onkeydown = function(e) {
 	var self = this;
 	return function(e) {
+		if(self.ignoreRepeats)
+			return;
+		self.ignoreRepeats=true;
 		var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
 		if(self.KEY_CODES[keyCode]) {
 			e.preventDefault();
@@ -31,6 +35,7 @@ InputManager.prototype.onkeydown = function(e) {
 InputManager.prototype.onkeyup = function(e) {
 	var self = this;
 	return function(e) {
+		self.ignoreRepeats=false;
 		var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
 		if(self.KEY_CODES[keyCode]) {
 			e.preventDefault();
