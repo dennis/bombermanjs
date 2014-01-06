313efbcf4503c8d5c1ead050b57e7b53e8d8bbf6
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2014-01-06 13:17:50 +0100

    Improve walking-detecting

--- public/js/player.js
+++ public/js/player.js
@@ -46,38 +46,46 @@ function Player(name, kind) {
 	this.newState = new ActorState();
 	this.realX = null;
 	this.realY = null;
-	this.logic_counter = 0;
+	this.lastSeenWalking = 0;
 
 	console.log("Created actor", name, kind);
 };
 Player.prototype = new Actor();
 Player.prototype.draw = function(context, tileSet, interpolation, ticks) {
-	if(this.realX != null && this.realY != null) {
-		tileSet.clear(context, this.realX, this.realY);
-	}
-	else {
+	if(this.realX == null && this.realY == null) {
 		this.realX = this.state.x;
 		this.realY = this.state.y;
+		this.newState = this.state;
 	}
 
 	if(!this.newState.visible)
 		return;
 
-	this.realX = this.newState.x - Math.floor((this.newState.x - this.state.x) * interpolation);
-	this.realY = this.newState.y - Math.floor((this.newState.y - this.state.y) * interpolation);
+	var distX = this.newState.x - this.state.x;
+	var distY = this.newState.y - this.state.y;
+
+	this.realX = this.newState.x - Math.floor(distX * interpolation);
+	this.realY = this.newState.y - Math.floor(distY * interpolation);
 
 	var direction = this.newState.direction;
 
-	if(this.newState.x != this.state.x || this.newState.y != this.state.y)
-		direction = 'walk-' + direction;
+	if(distX == 0 && distY == 0) {
+		;
+	}
+	else {
+		this.lastSeenWalking = ticks;
+	}
+	//if(this.newState.x != this.state.x || this.newState.y != this.state.y)
+		//direction = 'walk-' + direction;
 
+	if(ticks - this.lastSeenWalking < 250)
+		direction = 'walk-' + direction;
 	tile = this.kind.get(direction, ticks);
 
 	tileSet.draw(context, this.realX, this.realY, tile);
 }
 
 Player.prototype.logic = function() {
-	this.logic_counter++;
 	this.realX = this.state.x;
 	this.realY = this.state.y;
 	this.state.update(this.newState);
