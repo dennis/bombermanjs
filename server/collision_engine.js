689a89387279d5397e0884febf78ee0f633e039a
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2014-01-07 22:45:17 +0100

    Simplified collision_engine

--- server/collision_engine.js
+++ server/collision_engine.js
@@ -1,26 +1,13 @@
 function CollisionEngine(width, height) {
 	this.width = width;
 	this.height = height;
-	this.tiles = [];
-
-	// initialize tiles
-	for(var x = 0; x < this.width; x++) {
-		this.tiles.push([]);
-		for(var y = 0; y < this.height; y++) {
-			this.tiles[x].push([]);
-		}
-	}
-	for(var x = 0; x < this.width; x++) {
-		for(var y = 0; y < this.height; y++) {
-			this.tiles[x][y] = false;
-		}
-	}
+	this.cells = new Array(width*height);
 }
 CollisionEngine.prototype.setXY = function(x, y) {
-	this.tiles[x][y] = true;
+	this.cells[y*this.width + x] = true;
 };
 CollisionEngine.prototype.isBlocked = function(x, y) {
-	return this.tiles[x][y];
+	return this.cells[y*this.width + x];
 }
 
 module.exports = CollisionEngine;
