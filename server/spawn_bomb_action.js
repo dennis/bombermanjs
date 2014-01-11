6ac760af0ca8c312e34897a954d78d2206102c05
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2014-01-11 01:54:02 +0100

    Snap bomb to a tile

--- server/spawn_bomb_action.js
+++ server/spawn_bomb_action.js
@@ -5,7 +5,23 @@ function SpawnBombAction(x, y) {
 	this.y = y;
 }
 SpawnBombAction.prototype.execute = function(player, world, update) {
-	world.dropBombAt(this.x, this.y);
+	var lowX = Math.floor(this.x / world.level.getTileWidth());
+	var lowY = Math.floor(this.y / world.level.getTileHeight());
+	var highX = Math.ceil(this.x / world.level.getTileWidth());
+	var highY = Math.ceil(this.y / world.level.getTileHeight());
+
+	// Determine the best location for bomb (snap to a tile)
+	var x = highX;
+	var y = highY;
+	if(this.x - lowX < highX - this.x)
+		x = lowX;
+	if(this.y - lowY < highY - this.y)
+		y = lowY;
+
+	x *= world.level.getTileWidth();
+	y *= world.level.getTileHeight();
+		
+	world.dropBombAt(x, y);
 }
 
 module.exports = SpawnBombAction;
